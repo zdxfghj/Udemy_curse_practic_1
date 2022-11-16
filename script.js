2bf7bd41ad9efa5189887febafc136037f3248dc
Vladislau <dolganovvlad486@gmail.com>
2022-11-16 19:54:19 +0300

    modify algorihm

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
 
 
 let numberOfFilms = +prompt("How many film you watched?","");
-
 let personalMovieDB = {
     count: numberOfFilms,
-    movies: {
-
-    },
-    actors: {
-
-    },
+    movies: {},
+    actors: {},
     genres:[],
     privat: false,
 };
+for(let i = 0; i<2;i++){
+    let a = prompt("What the last film you watched?",""),
+        b = +prompt("description film you watched?","");
+        if( a === '' || a.length >= 50 || a === null)
+          {
+            a = prompt("What the last film you watched?","");
+          } 
+          else  if( b === '' || b.length >= 50 || a === null)
+          {
+            b = prompt("What the last film you watched?","");
+          } else
+                {personalMovieDB.movies[a] = b; }
+}
 
 
-const a = prompt("What the last film you watched?",""),
-      b = +prompt("description film you watched?",""),
-      c = prompt("What the last film you watched?",""),
-      d = +prompt("description film you watched?","");
-      personalMovieDB.movies[a] = b;
-      personalMovieDB.movies[c] = d;
-
-
+if(personalMovieDB.count<10){
+    alert("Просмотрено довольно мало фильмов");
+}
+else if(personalMovieDB.count>=10 && personalMovieDB.count<=30){
+    alert("Вы класический зритель");
+}
+else if(personalMovieDB.count > 30){
+    alert("Вы киноман");
+}else{
+    alert("error");
+}
+        
 console.log(personalMovieDB);
