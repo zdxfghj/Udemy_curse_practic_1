b7281fbeca436351e9f67888b13467adf6ed4365
Vladislau <dolganovvlad486@gmail.com>
2022-11-18 19:39:16 +0300

    add function

--- script.js
+++ script.js
@@ -1,38 +1,77 @@
 
 
-let numberOfFilms = +prompt("How many film you watched?","");
+let numberOfFilms;
+ //   start();
+
 let personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
     genres:[],
-    privat: false,
+    privat: true,
 };
-for(let i = 0; i<2;i++){
-    let a = prompt("What the last film you watched?",""),
-        b = +prompt("description film you watched?","");
-        if( a === '' || a.length >= 50 || a === null)
-          {
-            a = prompt("What the last film you watched?","");
-          } 
-          else  if( b === '' || b.length >= 50 || a === null)
-          {
-            b = prompt("What the last film you watched?","");
-          } else
-                {personalMovieDB.movies[a] = b; }
+
+//rememberMyFilms();
+   // detectPersonalLevel();
+   // console.log(personalMovieDB);
+     showMyDB(personalMovieDB.privat);
+    writeYourGender();
+    console.log(personalMovieDB);
+
+
+
+function writeYourGender(){
+    for(let i=1; i<=3; i++){
+            personalMovieDB.genres[i-1] = prompt(`Ваш любимый жанр под номером ${i}`,'');
+    }
+   
+}
+
+    function showMyDB(hidden){
+    if( !hidden ){
+        console.log(personalMovieDB);}
 }
 
 
-if(personalMovieDB.count<10){
-    alert("Просмотрено довольно мало фильмов");
+function detectPersonalLevel(){
+
+    if(personalMovieDB.count<10){
+        alert("Просмотрено довольно мало фильмов");
+    }
+    else if(personalMovieDB.count>=10 && personalMovieDB.count<=30){
+        alert("Вы класический зритель");
+    }
+    else if(personalMovieDB.count > 30){
+        alert("Вы киноман");
+    }else{
+        alert("error");
+    }
+      
 }
-else if(personalMovieDB.count>=10 && personalMovieDB.count<=30){
-    alert("Вы класический зритель");
+
+
+
+
+function rememberMyFilms(){
+
+    for(let i = 0; i<2;i++){
+        let a = prompt("What the last film you watched?",""),
+            b = +prompt("description film you watched?","");
+            if( a === '' || a.length >= 50 || a === null)
+              {
+                a = prompt("What the last film you watched?","");
+              } 
+              else  if( b === '' || b.length >= 50 || a === null)
+              {
+                b = prompt("What the last film you watched?","");
+              } else
+                    {personalMovieDB.movies[a] = b; }
+    }
 }
-else if(personalMovieDB.count > 30){
-    alert("Вы киноман");
-}else{
-    alert("error");
+
+function start(){
+    numberOfFilms = +prompt("How many film you watched?","");
+    while( numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms) ){
+        numberOfFilms = +prompt("How many film you watched?","");
+    }
 }
-        
-console.log(personalMovieDB);
