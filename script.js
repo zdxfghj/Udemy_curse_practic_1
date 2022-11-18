6cf50ac7d36e0f44c58b02bcb62967128bde2575
Vladislau <dolganovvlad486@gmail.com>
2022-11-18 22:43:44 +0300

    add math.function

--- script.js
+++ script.js
@@ -11,14 +11,41 @@ let personalMovieDB = {
     privat: true,
 };
 
-//rememberMyFilms();
-   // detectPersonalLevel();
-   // console.log(personalMovieDB);
-     showMyDB(personalMovieDB.privat);
-    writeYourGender();
-    console.log(personalMovieDB);
+    //rememberMyFilms();
+    // detectPersonalLevel();
+    // console.log(personalMovieDB);
+    // showMyDB(personalMovieDB.privat);
+    // writeYourGender();
+    // console.log(personalMovieDB);
+
+    // calculateVolumeAndArea(15);
+    // calculateVolumeAndArea(-15);
+    // calculateVolumeAndArea(15.5);
+    // calculateVolumeAndArea('15');
+
+    // getCoupeNumber(33);
+    // getCoupeNumber(7);
+    // getCoupeNumber(300) ;
+    // getCoupeNumber(0) ;
+    // getCoupeNumber(7.7) ;
+    // getCoupeNumber(-10);
+    // getCoupeNumber('Hello');
+
+    // console.log(getTimeFromMinutes(150));
+    // console.log(getTimeFromMinutes(50)) ;
+    // console.log(getTimeFromMinutes(0)) ;
+    // console.log(getTimeFromMinutes(-150)) ;
 
 
+    // console.log(findMaxNumber(1, 5, 6.6, 11)) ;
+    // console.log(findMaxNumber(1, 5, '6', '10')) ;
+
+
+     console.log(fib(4)) ;
+     console.log(fib(7)) ;
+     console.log(fib('7')) ;
+     console.log(fib(1)) ;
+     console.log(fib(0)) ;
 
 function writeYourGender(){
     for(let i=1; i<=3; i++){
@@ -75,3 +102,76 @@ function start(){
         numberOfFilms = +prompt("How many film you watched?","");
     }
 }
+
+function calculateVolumeAndArea(lengthCub) {
+    if( typeof(lengthCub) === 'string' || lengthCub <= 0 || lengthCub % 1 !== 0 ){
+        console.log("При вычислении произошла ошибка");
+    }else{
+        console.log(`Обьем куба: ${Math.pow(lengthCub,3)}, площадь всей поверхности: ${Math.pow(lengthCub,2)*6}`);
+    }
+}
+
+// Место для второй задачи
+function getCoupeNumber(seating) {
+    if( typeof(seating) === 'string' || seating <= 0 || seating % 1 !== 0 ){
+        console.log("Ошибка. Проверьте правильность введенного номера места");
+    }else if( 0 < seating  && seating >= 36) {
+       console.log("Таких мест в вагоне не существует");
+    }else{
+        console.log( Math.ceil(seating/4));
+    }
+}
+
+function getTimeFromMinutes(minite) {
+    let str = '';
+    if( minite >= 0 && minite < 60 || minite >= 300 && minite <= 600){
+        str = 'ов';
+        return `Это ${Math.floor(minite/60)} час${str} и ${minite%60} минут`;
+    }else if(minite >= 60 && minite < 300 ){
+        str = 'а';
+        return `Это ${Math.floor(minite/60)} час${str} и ${minite%60} минут`;
+    } else{
+        return "Ошибка, проверьте данные";
+    }
+}
+
+
+function findMaxNumber(num1, num2,num3,num4) {
+        const arr = [num1, num2,num3,num4];
+        let max = 0 ;
+        for ( let i = 0; i<4; i++) {
+            if( typeof(arr[i]) === "string" || arr[i] == undefined ) 
+            {return 0;}
+            else{
+                if(arr[i] >  max){ max = arr[i];}
+            }
+        }
+        return max;
+}
+
+function fib(col) {
+    let arr =[];
+    let num1 = 0;
+    let num2 = 1;
+if(typeof(col) !== 'string'){
+    if(col == 1){
+        return `${num1}`;
+    }else if(col == 2 ){
+        return `${num2}`;
+    }else if(col == 0 ){
+        return '';
+    }else{
+        arr[0] = num1;
+        arr[1] = num2;
+        for(let i = 2; i < col; i++){
+            arr[i]= arr[i-2]+arr[i-1];
+        }
+    }
+   
+    return  arr.toString;
+}else{
+    return '';
+}
+     
+}
+
